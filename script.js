2ac2a8506dc77d3a8bf5b55beabb0c9d30d1013c
Rychardson360 <99352191+Rychardson360@users.noreply.github.com>
2022-03-23 16:33:36 -0300

    Exercício

---
+++ script.js
@@ -0,0 +1,20 @@
+function pegarDados1(){
+	let name = document.getElementsByTagName("button").value;
+	document.getElementById("quad").innerHTML = "Fique em casa, peça para fazerem as compras por você!";
+
+}
+function pegarDados2(){
+	let name = document.getElementsByTagName("button").value;
+	document.getElementById("quad").innerHTML = "Fique em casa, tome sol e faça exercícios leves!";
+
+}
+function pegarDados3(){
+	let name = document.getElementsByTagName("button").value;
+	document.getElementById("quad").innerHTML = "Fique em casa e cuide da sua saúde!";
+
+}
+function pegarDados4(){
+	let name = document.getElementsByTagName("button").value;
+	document.getElementById("quad").innerHTML = "Faça compras pelos mais idosos, mas use máscara!";
+
+}
