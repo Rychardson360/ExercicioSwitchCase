67029bbf4c79d5400e1fa567a2360a899013974a
Rychardson360 <99352191+Rychardson360@users.noreply.github.com>
2022-03-26 20:37:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-function pegarDados1(){
-	let name = document.getElementsByTagName("button").value;
-	document.getElementById("quad").innerHTML = "Fique em casa, peça para fazerem as compras por você!";
-
+function pegarDados(qualBotao){
+	switch (qualBotao) {
+        case 'Você é idoso?':{
+        let div = document.getElementById("botaoSelecionado");
+        div.classList.remove("quad");
+        div.classList.add("lucas");
+        document.getElementById("botaoSelecionado").innerHTML = "Fique em casa, peça para fazerem as compras para você!";
+        break;
+        }
+        case 'Sofre de problemas respiratórios?':{
+        let div = document.getElementById("botaoSelecionado");
+        div.classList.remove("quad");
+        div.classList.add("lucas");
+        document.getElementById("botaoSelecionado").innerHTML = "Fique em casa, tome sol e faça exercícios leves!";
+        break;
+        }
+        case 'É diabético?':{
+        let div = document.getElementById("botaoSelecionado");
+        div.classList.remove("quad");
+        div.classList.add("lucas");
+        document.getElementById("botaoSelecionado").innerHTML = "Fique em casa e cuide da sua saúde!";
+        break;
+        }
+        case 'É saudável?':{
+        let div = document.getElementById("botaoSelecionado");
+        div.classList.remove("quad");
+        div.classList.add("lucas");
+        document.getElementById("botaoSelecionado").innerHTML = "Faça compras pelos mais idosos, mas use máscaras!";
+        break;
+        }
+        default:
+        document.getElementById("botaoSelecionado").innerHTML = "erro";
+    }
 }
-function pegarDados2(){
-	let name = document.getElementsByTagName("button").value;
-	document.getElementById("quad").innerHTML = "Fique em casa, tome sol e faça exercícios leves!";
-
-}
-function pegarDados3(){
-	let name = document.getElementsByTagName("button").value;
-	document.getElementById("quad").innerHTML = "Fique em casa e cuide da sua saúde!";
-
-}
-function pegarDados4(){
-	let name = document.getElementsByTagName("button").value;
-	document.getElementById("quad").innerHTML = "Faça compras pelos mais idosos, mas use máscara!";
-
-}
+let ident = "Desenvolvido por Lucas Rychardson 2º Ano Informática";
+console.log(ident);
